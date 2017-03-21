bd196ab4ea6bd2f4140fb718df2b4f9de26cea33
Liane Stevenson <webdevelopwolf@gmail.com>
2017-03-21 23:38:30 +0000

    Static Modal
    Basic Static Modal Source css & JS files Added

---
+++ source/js/akela.js
@@ -0,0 +1,31 @@
+// Open modal
+function openModal(clicked_id) {
+  var button = document.getElementById(clicked_id);
+  var modal = button.getAttribute("data-modal");
+  var modalElement = document.getElementById(modal);
+  modalElement.className += " modal-open";
+  var backdrop = document.createElement('div');
+  backdrop.id="modal-backdrop";
+  backdrop.classList.add("modal-backdrop");
+  document.body.appendChild(backdrop);
+  var backdrop = document.getElementById("modal-backdrop");
+  backdrop.className += " modal-open";
+  // Close Same Modal Event Handlers
+  (function() {
+    document.getElementById("modal-close").onclick = function(e) {
+      closeModal(modalElement, backdrop);
+    }
+    document.getElementById("close").onclick = function(e) {
+      closeModal(modalElement, backdrop);
+    }
+    document.getElementById("modal-backdrop").onclick = function(e) {
+      closeModal(modalElement, backdrop);
+    }
+  })();
+}
+
+// Close Modal
+function closeModal (modalElement, backdrop) {
+  modalElement.className = modalElement.className.replace(/\bmodal-open\b/, '');
+      backdrop.className = backdrop.className.replace(/\bmodal-open\b/, '');
+}
